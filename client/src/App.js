adf9087691326b3cfd96ce717a30beb5f5806056
Lucy Qi <l8qi@uwaterloo.ca>
2024-01-13 15:33:25 -0500

    made containers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import {CTA, Brand, Navbar} from './components';
+
 
 
 const App = () => {
   return (
-    <div>
-        <h1> testing</h1>
+    <div classname ="App">
+      <div className = "gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features />
+      <Possibility />
+      <CTA/>
+      <Blog/>
+      <Footer/>
+        
     </div>
   )
 }
