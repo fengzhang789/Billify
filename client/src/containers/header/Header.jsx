7585babbedb0381ac9a4dd2656fcdd263030a327
Lucy Qi <l8qi@uwaterloo.ca>
2024-01-13 18:43:27 -0500

    finished landing page

--- client/src/containers/header/Header.jsx
+++ client/src/containers/header/Header.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from 'react';
+
+import bill from '../../assets/bill.png';
 import './header.css';
 
 
-const Header = () => {
-  return (
-    <div className="billify__header section__padding" id="home">
-        <div className="billify__header-content"></div>
-        <h1 className="gradient__text">Revolutionize doctor's workflow by automating codes</h1>
-    </div>
-  )
-}
+const Header = () => (
+  <div className="billify__header section__padding" id="home">
+    <div className="billify__header-content">
+      <h1 className="gradient__text">Revolutionize Doctor's Workflow by Automating Codes</h1>
+      <p>Billify streamlines medical billing by converting SOAP notes into billing records, providing doctors with efficient coding and bill generation capabilities.</p>
 
-export default Header
+      <div className="billify__header-content__input">
+        
+        <button type="button">Get Started</button>
+      </div>
+
+
+    </div>
+
+    <div className="billify__header-image">
+      <img src={bill} />
+    </div>
+    
+  </div>
+);
+
+export default Header;
