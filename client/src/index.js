c2f23a535e792dc2e47e9a4f31a8041f94ca4161
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-01-14 05:00:04 -0500

    removed errors from app

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
-import NewApp from "./NewApp"
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
+import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-            <NewApp />
-    </React.StrictMode>
-    , document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
