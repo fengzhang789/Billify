9a0972ad9ef7b5997c83eeb65d671bd8524b8ffa
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-01-14 05:03:06 -0500

    removed two index.js

--- client/src/components/index.js
+++
@@ -1,4 +0,0 @@
-
-export { default as Feature } from './feature/Feature';
-export { default as Navbar } from './navbar/Navbar';
-export { default as SOAPNotesForm } from './SOAPNotesForm/SOAPNotesForm';
